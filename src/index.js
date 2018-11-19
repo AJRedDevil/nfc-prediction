2ddf11f39719806d18715bffb482caf4c38a43c1
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-11-19 19:08:57 +0545

    Add 404 and 500 server error

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+// eslint-disable-next-line no-unused-vars
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use(function(req, res, next) {
+  res.status(404).send("Sorry can't find that!");
+});
+
 app.listen(PORT, () =>
   console.log(`NFC Prediction server listening on port ${PORT}!`)
 );
